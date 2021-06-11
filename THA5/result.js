9611d2fe0195c1fa67f9ffd40c297a096934636c
Pranav1702 <61620010+Pranav1702@users.noreply.github.com>
2021-06-12 00:06:00 +0530

    Javascript

---
+++ THA5/result.js
@@ -0,0 +1,7 @@
+var x='w3resourse';
+var y=[1,2,3,4];
+function is_array(){
+console.log (Array.isArray(x));
+console.log (Array.isArray(y));
+}
+is_array();
---
+++ THA5/result3.js
@@ -0,0 +1,9 @@
+function is(x){
+    return x.join(",");
+}
+function are(x){
+    return x.join("+");
+}
+console.log('"'+is(["Red", "Green", "White", "Black"])+'"');
+console.log('"'+is(["Red", "Green", "White", "Black"])+'"');
+console.log('"'+are(["Red", "Green", "White", "Black"])+'"');
