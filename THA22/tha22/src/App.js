def357051d68cefbfb278112227273a22d4f36cc
Pranav1702 <61620010+Pranav1702@users.noreply.github.com>
2021-07-19 23:54:03 +0530

    Meme Generator

--- THA22/tha22/src/App.js
+++ THA22/tha22/src/App.js
@@ -1,16 +1,17 @@
 //import logo from './logo.svg';
-import { useState,useEffect } from 'react';
+import React,{ useState,useEffect } from 'react';
 import Template from './Template';
 import Meme from './Meme';
 import './App.css';
+
 
 function App() {
 const [Memes,setMemes]=useState([]);
 const [yash,setYash]=useState(null);
 useEffect(()=>{
   fetch("https://api.imgflip.com/get_memes")
-  .then(res=>res.json())
-  .then(data => {
+   .then(res=>res.json())
+   .then(data => {
     setMemes(data.data.memes); 
   });
 },[]);
@@ -18,7 +19,7 @@ useEffect(()=>{
     <div className="App">
 <h1>Meme Generator</h1>
 {yash===null?(<Template Memes={Memes} setYash={setYash}/>):(<Meme yash={yash} setYash={setYash}/>)}
-   
+
   </div>
   );
 }
